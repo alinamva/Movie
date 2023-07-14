4570e2732dc89eafd652c9c73a407a0e54d184d0
unknown <alinamva>
2023-07-14 12:53:06 +0400

    first commit

--- src/layouts/Master.jsx
+++ src/layouts/Master.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Master(params) {
   return (
     <>
-    <Header />
-            {params.children}
-            <Footer />
-</> 
- )
+      <Header />
+      {params.children}
+      <Footer />
+    </>
+  );
 }
